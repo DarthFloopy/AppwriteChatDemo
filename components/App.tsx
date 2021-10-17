623d27d1b88678ad6af4804b1e29a59cba046452
Joseph Cagle <joseph@josephcagle.com>
2021-10-17 19:49:35 -0400

    Move reset css to plain css file

--- components/App.tsx
+++ components/App.tsx
@@ -1,18 +1,8 @@
 
 import React from 'react'
-import styled from 'styled-components'
 import LoginBox from './LoginBox'
 
-const CSSResetDiv = styled.div`
-    margin: 0;
-    padding: 0;
-    font-family: Helvetica, sans-serif;
-    font-size: 1.5rem;
-`
-
 export default function App() {
-    return <CSSResetDiv>
-        <LoginBox />
-    </CSSResetDiv>
+    return <LoginBox />
 }
 
