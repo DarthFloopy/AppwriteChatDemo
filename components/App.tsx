acd5cadc4b6ce4291953dae7c7ad113741423cfc
Joseph Cagle <joseph@josephcagle.com>
2021-10-17 20:27:57 -0400

    Send user auth request on login form submit

--- components/App.tsx
+++ components/App.tsx
@@ -1,10 +1,20 @@
 
-import React from 'react'
+import { Appwrite } from "appwrite";
+import React from "react";
 import LoginBox from './LoginBox'
 
 export default function App() {
     return <LoginBox onSubmit={(usernameValue, passwordValue) => {
-        console.log(usernameValue, passwordValue)
+        const appwrite = new Appwrite()
+
+        appwrite
+            .setEndpoint('http://localhost:8080/v1')
+            .setProject('616c95936708c')
+
+        appwrite.account.createSession(usernameValue, passwordValue)
+            .then(response => console.log(response))
+            .catch(console.log)
+
     }} />
 }
 
--- index.tsx
+++ index.tsx
@@ -1,15 +1,8 @@
 
-import { Appwrite } from 'appwrite'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './components/App'
-
-const appwrite = new Appwrite()
-
-appwrite
-    .setEndpoint('http://localhost:8080/v1')
-    .setProject('616c95936708c')
 
 ReactDOM.render(<App />,
         document.getElementById("app-root"))
