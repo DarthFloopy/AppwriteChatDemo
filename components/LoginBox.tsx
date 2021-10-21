7529382f9d1a0b9c17cbd51fd2a8d9cedda61da0
Joseph Cagle <joseph@josephcagle.com>
2021-10-21 18:58:30 -0400

    Extract new Heading component

--- components/LoginBox.tsx
+++ components/LoginBox.tsx
@@ -1,20 +1,15 @@
 
 import React, { useState } from 'react'
+import Heading from './Heading'
 import Button from './Button'
 import TextInput from './TextInput'
-
-import styled from 'styled-components'
-
-const StyledH2 = styled.h2`
-    margin: 0.5rem;
-`
 
 export default function LoginBox({ onSubmit }) {
     const [usernameValue, setUsernameValue] = useState("")
     const [passwordValue, setPasswordValue] = useState("")
 
     return <div style={{"display": "flex", "flexDirection": "column"}}>
-        <StyledH2>Log In</StyledH2>
+        <Heading as="h2">Log In</Heading>
         <TextInput label="Username" name="username" onChange={({value}) => {
             setUsernameValue(value)
         }} />
