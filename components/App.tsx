b31db5badf1de158a49e8c48fc7cdd5bcec3da66
Joseph Cagle <joseph@josephcagle.com>
2021-10-23 12:22:38 -0400

    Add dbapis.ts to help with DB I/O

--- components/App.tsx
+++ components/App.tsx
@@ -1,10 +1,14 @@
 
 import React from "react";
+import { getRoomsDirectory, createNewRoom, deleteRoom } from "../dbapis";
 import JoinRoomBox from "./JoinRoomBox";
 
 export default function App() {
 
-    return <JoinRoomBox onSubmit={console.log}></JoinRoomBox>
+    return <JoinRoomBox onSubmit={ (name, roomID) => {
+        createNewRoom(roomID).then(console.log)
+    }}></JoinRoomBox>
+
 
 }
 
---
+++ dbapis.ts
@@ -0,0 +1,56 @@
+
+import { Appwrite } from "appwrite";
+
+const sdk = new Appwrite()
+sdk
+    .setEndpoint('http://localhost:8080/v1')
+    .setProject('616c95936708c')
+
+const roomsDirectoryCollectionID = "61734789324d1"
+const chatDataCollectionID = "61733d70ceb7f"
+
+export async function getRoomsDirectory() {
+    const response = await sdk.database.listDocuments(roomsDirectoryCollectionID)
+    const result = {}
+    for (const doc of response["documents"]) {
+        result[doc["roomName"]] = doc["roomID"]
+    }
+    return result
+}
+
+export async function createNewRoom(roomName) {
+    const dir = await getRoomsDirectory()
+    if (dir.hasOwnProperty(roomName)) {
+        throw Error(`room already exists with name: ${roomName}`)
+    }
+
+    const newDoc = await sdk.database.createDocument(chatDataCollectionID, {
+        roomName: roomName,
+        messages: []
+    })
+    await sdk.database.createDocument(roomsDirectoryCollectionID, {
+        roomName: roomName,
+        roomID: newDoc["$id"]
+    })
+
+    return newDoc
+}
+
+export async function deleteRoom(roomName) {
+    const roomsDirectoryCollection =
+        await sdk.database.listDocuments(roomsDirectoryCollectionID)
+
+    const chatDataCollection =
+        await sdk.database.listDocuments(chatDataCollectionID)
+
+    // remove chat rooms
+    for (const doc of chatDataCollection.documents.filter(doc => (doc.roomName == roomName))) {
+        sdk.database.deleteDocument(chatDataCollectionID, doc["$id"])
+    }
+
+    // remove entries from rooms directory
+    for (const doc of roomsDirectoryCollection.documents.filter(doc => (doc.roomName == roomName))) {
+        sdk.database.deleteDocument(roomsDirectoryCollectionID, doc["$id"])
+    }
+}
+
