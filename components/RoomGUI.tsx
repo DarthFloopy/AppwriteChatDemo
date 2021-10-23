6c8f214a3f3f4ab5931c4e214f67f51fd41e3769
Joseph Cagle <joseph@josephcagle.com>
2021-10-23 16:41:31 -0400

    Give RoomGUI lots of updates

--- components/RoomGUI.tsx
+++ components/RoomGUI.tsx
@@ -1,7 +1,38 @@
 
 import React, { useEffect, useState } from "react"
 import styled from "styled-components"
+import Button from "./Button"
 import { getMessages } from "../dbapis"
+
+
+const Container = styled.div`
+    margin: 0;
+    padding: 0.5rem;
+`
+
+const MessagesView = styled.div`
+    outline: solid 1px darkgray;
+    padding: 0.5rem;
+    max-height: 80vh;
+    overflow-y: scroll;
+`
+
+const MessageInputContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-top: 0.5rem;
+`
+
+const MessageInput = styled.input`
+    padding: 0.5rem;
+    flex-grow: 1;
+    font-size: 1rem;
+`
+const MessageSendButton = styled(Button)`
+    margin: 0;
+    margin-left: 0.5rem;
+`
+
 
 export default function RoomGUI({ roomName }) {
 
@@ -14,11 +45,15 @@ export default function RoomGUI({ roomName }) {
         })
     }, [])
 
-    return <>
-        <div> {
+    return <Container>
+        <MessagesView> {
             messagesText.split("\n").map(
                 line => <p key={line}>{line}</p>
             )
-        } </div>
-    </>
+        } </MessagesView>
+        <MessageInputContainer>
+            <MessageInput name="messageinput" id="messageinput" />
+            <MessageSendButton>➤</MessageSendButton>
+        </MessageInputContainer>
+    </Container>
 }
