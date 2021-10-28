99a69134b53698ae03be72d8ab722c571c625a0d
Joseph Cagle <joseph@josephcagle.com>
2021-10-27 22:18:58 -0400

    Add a dialog for user to confirm when starting a new room

--- components/App.tsx
+++ components/App.tsx
@@ -1,6 +1,7 @@
 
 import React, { useState } from "react";
 import { deleteRoom, getAllRoomNames } from "../dbapis";
+import Dialog from "./Dialog";
 import JoinRoomBox from "./JoinRoomBox";
 import RoomGUI from "./RoomGUI"
 
@@ -9,20 +10,36 @@ export default function App() {
     const [roomName, setRoomName] = useState(null)
     const [userName, setUserName] = useState(null)
     const [userJoinedRoom, setUserJoinedRoom] = useState(false)
+    const [dialogVisible, setDialogVisible] = useState(false)
 
     if (!userJoinedRoom) {
-        return <JoinRoomBox onSubmit={ (name, roomName) => {
-            setUserName(name)
-            setRoomName(roomName)
-            setUserJoinedRoom(true)
+        return <div style={{"position":"relative"}}>
+            <JoinRoomBox onSubmit={ (name, roomName) => {
+                setUserName(name)
+                setRoomName(roomName)
 
-            getAllRoomNames().then(list => {
-                console.log(list)
-                if (!(list.includes(roomName))) {
-                    console.log("room does not exist")
-                }
-            })
-        }} />
+                getAllRoomNames().then(list => {
+                    if (list.includes(roomName)) {
+                        setUserJoinedRoom(true)
+                    } else {
+                        setDialogVisible(true)
+                    }
+                })
+            }} />
+            {
+                dialogVisible &&
+                    <Dialog
+                        text={`The room "${roomName}" does not exist. Create it?`}
+                        buttonLabels={["Yes", "No"]}
+                        onClick={response => {
+                            setDialogVisible(false)
+                            if (response == "Yes") {
+                                setUserJoinedRoom(true)
+                            }
+                        }}
+                        />
+            }
+        </div>
     }
     else {
         return <RoomGUI roomName={roomName} userName={userName} />
