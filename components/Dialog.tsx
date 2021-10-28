f459eac44f34482561fef2678a9a7dbe1d0ff996
Joseph Cagle <joseph@josephcagle.com>
2021-10-27 22:17:47 -0400

    Add new Dialog component

---
+++ components/Dialog.tsx
@@ -0,0 +1,35 @@
+
+import React, { useEffect, useRef, useState } from "react"
+import styled from "styled-components"
+import Button from "./Button"
+
+const DialogDiv = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 1rem;
+    background: white;
+    border-radius: 8px;
+    border: solid 5px lightblue;
+    max-width: 90vw;
+    min-width: 50%;
+    text-align: center;
+    word-wrap: break-word;
+`
+
+export default function Dialog({ text, buttonLabels, onClick }) {
+    return <DialogDiv>
+        {text}
+        <br />
+        {buttonLabels.map(
+            (label, index) =>
+                <Button
+                    key={index}
+                    onClick={() => { onClick(label) }}
+                    >{label}</Button>
+        )}
+    </DialogDiv>
+
+}
+
